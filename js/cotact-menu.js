282247040051235db819ff99eaf5b7e092690a4e
Cloudia <dusyasha@gmail.com>
2020-10-30 09:10:21 +0200

    added a contact menu

---
+++ js/cotact-menu.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "230px";
+  document.getElementById("main").style.marginLeft = "230px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
