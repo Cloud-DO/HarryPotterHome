4810d919b76d02f2721e67e9c789c527b98fb0a8
Cloudia <dusyasha@gmail.com>
2020-11-11 13:08:14 +0200

    removed about button (for now)
    +added the news slider

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 10, 2020 18:00:00").getTime();
+var countDownDate = new Date("Nov 12, 2020 18:00:00").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function() {
